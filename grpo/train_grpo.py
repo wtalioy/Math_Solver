8304e515c0f1ba539ba481e803a3b71508d4a91d
wtalioy <2251065331@qq.com>
2025-05-12 22:24:59 +0800

    refactor: change qwen3 to qwen2.5

--- grpo/train_grpo.py
+++ grpo/train_grpo.py
@@ -1,21 +1,21 @@
 from unsloth import FastLanguageModel, PatchFastRL
 from trl import GRPOTrainer
 from swanlab.integration.transformers import SwanLabCallback
-from .data_utils import load_custom_dataset
-from .config import training_args
-from .reward_funcs import format_reward, tag_count_reward, accuracy_reward, len_reward, cosine_scaled_reward
+from data_utils import load_custom_dataset
+from config import training_args
+from reward_funcs import format_reward, tag_count_reward, accuracy_reward, len_reward, cosine_scaled_reward
 import os
 
 PatchFastRL("GRPO", FastLanguageModel)
 
 max_seq_length = 2048
-max_lora_rank = 64
+max_lora_rank = 32
 
 trainset_path = "data/train_10k.json"
 valset_path = "data/val.json"
 
 if "MODEL_PATH" not in os.environ:
-    os.environ["MODEL_PATH"] = "models/Qwen3-0.6B"
+    os.environ["MODEL_PATH"] = "./models/Qwen2.5-0.5B-Instruct"
 
 def main():
 
@@ -25,8 +25,9 @@ def main():
         max_seq_length=max_seq_length,
         max_lora_rank=max_lora_rank,
         load_in_4bit=True,
+        # load_in_8bit=True,
         fast_inference=True,
-        gpu_memory_utilization=0.8,
+        gpu_memory_utilization=0.9,
     )
 
     model = FastLanguageModel.get_peft_model(
@@ -34,7 +35,7 @@ def main():
         r=max_lora_rank,
         lora_alpha=16,
         lora_dropout=0.1,
-        target_modules=["query_key_value"],
+        target_modules=["q_proj", "v_proj", "k_proj", "o_proj", "gate_proj", "down_proj", "up_proj"],
     )
 
     # Load the dataset
@@ -43,8 +44,8 @@ def main():
 
     # Initialize the SwanLab callback
     swanlab_callback = SwanLabCallback(
-        project="Qwen3-0.6B-grpo",
-        experiment_name="Qwen3-0.6B-grpo",
+        project="Qwen2.5-0.5B-grpo",
+        experiment_name="Qwen2.5-0.5B-grpo",
         config={
             "model": os.environ["MODEL_PATH"],
             "dataset": "custom_dataset",
